485e1dac7b4d4dbf898bb5d8995c2947722d6ea2
Stipe <dikshantsinghyt@gmail.com>
2024-05-28 16:21:59 +0530

    Simple Smart Contracts

---
+++ SimpleStorage.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract structs{
+    // creating a struct
+    struct people{
+        int age;
+        string name;
+    }
+
+    // creating an array for it
+    people[] public listofPeople;
+
+    // manually adding first value
+    constructor() {
+        listofPeople.push(people(8, "alex"));
+    }
+
+    // creating a mapping to map from age to name
+    mapping(int => string) public ageToName;
+
+    // creating a function to add values manually according to user input
+    function manualAddition(int _age, string memory _name) public{
+        listofPeople.push(people(_age, _name));
+        ageToName[_age] = _name;
+    }
+}
